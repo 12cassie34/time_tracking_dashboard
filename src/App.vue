9c205b0bb66aa491e57d2792f1ade7fea4f15caf
Shan Yu Lin <ineslin35@gmail.com>
2021-11-28 13:54:23 +0800

    feat: add TheWork component draft

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import TheProfile from "./components/TheProfile.vue";
+import TheWork from "./components/TheWork.vue";
 </script>
 
 <template>
   <div class="pt-14 pr-6 pb-14 pl-6 w-screen h-screen bg-blue-vary_dark font-rubik">
     <TheProfile />
+    <TheWork />
   </div>
 </template>
 
---
+++ src/components/TheWork.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="relative">
+    <div class="rounded-2xl flow-root overflow-hidden bg-orange">
+      <img
+        class="mr-4 relative bottom-2.5 float-right"
+        src="../assets/icon-work.svg"
+        alt="Work"
+      />
+    </div>
+    <div
+      class="info-container px-4 py-5 rounded-2xl grid grid-cols-2 bg-blue-dark"
+    >
+      <div class="text-white">
+        <div>Work</div>
+        <div class="font-light text-2xl">32 hrs</div>
+      </div>
+      <div
+        class="
+          flex flex-col
+          justify-around
+          items-end
+          text-blue-desaturated_blue
+        "
+      >
+        <img class="" src="../assets/icon-ellipsis.svg" alt="ETC" />
+        <div class="text-sm">Last Week - 36hrs</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.info-container {
+  transform: translateY(-40px);
+}
+</style>
